fc955946541dec60e5c9c22472df7a54cf339f9e
sonam-niit <sonamlovesmumypapa@gmail.com>
2023-09-02 09:43:20 +0530

    node mailer updated

--- Express/Session-38/config.js
+++ Express/Session-38/config.js
@@ -3,11 +3,9 @@ require('dotenv').config();
 
 const transporter= nodemailer.createTransport({
     service:'gmail',
-    host: "smtp.gmail.com",
-    port:587,
     auth: {
-        user: "sonam.gravity@gmail.com",
-        pass: "rqddzawbfmmifmro"
+        user: process.env.GMAIL_ID,
+        pass: process.env.PASSWORD
     }
 })
 
--- Express/Session-38/controllers/auth.controller.js
+++ Express/Session-38/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 const bcrypt = require('bcrypt');
-const jwt= require('jsonwebtoken');
-const dotenv= require('dotenv').config();
+const nodemailer = require("nodemailer");
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv').config();
 const UserModel = require('../models/user.model');
-const { transporter } = require('../config');
+const transporter = require('../config');
 const register = async (req, res) => {
 
     const { name, email, username, password } = req.body;
@@ -10,67 +11,76 @@ const register = async (req, res) => {
         if (!name || !email || !password || !username) {
             return res.status(403).send({ message: "Fields can not be empty" })
         }
-        let user =await UserModel.findOne({ email });
+        let user = await UserModel.findOne({ email });
         if (user) {
             return res.status(403).send({ message: "email is already registred" })
         }
-        user =await UserModel.findOne({ username });
+        user = await UserModel.findOne({ username });
         if (user) {
             return res.status(403).send({ message: "Username is already taken by Someone" })
         }
 
         const hashPassword = await bcrypt.hash(password, 12);
-        const newUser = new UserModel({name, email, username, password: hashPassword});
+        const newUser = new UserModel({ name, email, username, password: hashPassword });
 
-        const resp=await newUser.save();
+        const resp = await newUser.save();
         //Send Registration success Email
-        const mailOptions={
-            from:process.env.GMAIL_ID,
+
+        // let transporter = nodemailer.createTransport({
+        //     service: 'gmail',
+        //     auth: {
+        //         user: process.env.EMAIL,
+        //         pass: process.env.PASSWORD
+        //     }
+        // })
+        const mailOptions = {
+            from: process.env.GMAIL_ID,
             to: email,
-            subject:"Registration Successful",
-            text:"Congratulation..!!! You have successfully Registered"
+            subject: "Registration Successful",
+            text: "Congratulation..!!! You have successfully Registered"
 
         }
-        transporter.send(mailOptions,(err,info)=>{
-            if(error){
-                console.log("Error Sending Mail",error);
-            }else{
-                console.log("mail Sent");
+        transporter.sendMail(mailOptions, (err, info) => {
+            if (err) {
+                console.log("Error Sending Mail", error);
+            } else {
+                console.log("mail Sent",info);
                 // return res.status(201).send({ message: "User Registered Email Sent to your"+ 
                 // "registered Email Id", resp })
             }
         })
-        return res.status(201).send({ message: "User Registered",resp })
+        return res.status(201).send({ message: "User Registered", resp })
 
     } catch (error) {
         console.log(error);
     }
 }
 
-const login=async (req,res)=>{
+const login = async (req, res) => {
 
-    const {username, password } = req.body;
+    const { username, password } = req.body;
     try {
         if (!password || !username) {
             return res.status(403).send({ message: "Username and Password is Mandatory" })
         }
-       let user =await UserModel.findOne({ username });
+        let user = await UserModel.findOne({ username });
         if (!user) {
             return res.status(403).send({ message: "Username is not registered with Us" })
         }
-        else{
-            const resp=await bcrypt.compare(password,user.password);
-            const payload={
-                id:user._id,
-                name:user.name,
-                username:user.username
+        else {
+            const resp = await bcrypt.compare(password, user.password);
+            const payload = {
+                id: user._id,
+                name: user.name,
+                username: user.username
             }
-            if(resp){
-                const jwttoken= jwt.sign(payload,process.env.JWT_SECRET);
-                return res.status(200).send({ message: "Successfully Logged In"
-                ,userDetails:payload,token:jwttoken
-            })
-            }else{
+            if (resp) {
+                const jwttoken = jwt.sign(payload, process.env.JWT_SECRET);
+                return res.status(200).send({
+                    message: "Successfully Logged In"
+                    , userDetails: payload, token: jwttoken
+                })
+            } else {
                 return res.status(403).send({ message: "Incorrect Password" })
             }
         }
@@ -80,4 +90,4 @@ const login=async (req,res)=>{
     }
 
 }
-module.exports={register,login}
+module.exports = { register, login }
